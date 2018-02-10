45793cc9c80465d525bcbbe892dc885d2435c0e4
Clint Armstrong <clint@clintarmstrong.net>
2018-02-09 22:42:29 -0500

    support getorcreate if only macvlan does not exist

--- hostInterface/hi.go
+++ hostInterface/hi.go
@@ -16,83 +16,78 @@ type HostInterface struct {
 	log  *log.Entry
 }
 
-func NewHostInterface(name string, gateway *net.IPNet, opts map[string]string) (*HostInterface, error) {
+func GetOrCreateHostInterface(name string, gateway *net.IPNet, opts map[string]string) (*HostInterface, error) {
 	log := log.WithField("HostInterface", name)
-	log.Debug("NewHostInterface")
-	vxl, err := vxlan.NewVxlan(name, opts)
-	if err != nil {
-		log.WithError(err).Debug("failed to create vxlan")
-		return nil, err
-	}
+	log.Debug("GetOrCreateHostInterface")
+	hi, _ := getHostInterface(name)
+	log = hi.log
 
-	mvl, err := vxl.CreateMacvlan("hmvl_" + name)
-	if err != nil {
-		log.WithError(err).Debug("failed to create host macvlan")
-		err2 := vxl.Delete()
-		if err2 != nil {
-			log.WithError(err).WithError(err2).Debug("failed to delete vxlan")
-			return nil, err2
+	var err error
+	if hi.vxl == nil {
+		hi.vxl, err = vxlan.NewVxlan(name, opts)
+		if err != nil {
+			log.WithError(err).Debug("failed to create vxlan")
+			return nil, err
 		}
-		return nil, err
 	}
 
-	err = mvl.AddAddress(gateway)
+	if hi.mvl == nil {
+		hi.mvl, err = hi.vxl.CreateMacvlan("hmvl_" + name)
+		if err != nil {
+			err2 := hi.vxl.Delete()
+			if err2 != nil {
+				log.WithError(err).WithError(err2).Debug("failed to delete vxlan")
+				return nil, err2
+			}
+			return nil, err
+		}
+	}
+
+	err = hi.mvl.AddAddress(gateway)
 	if err != nil {
 		log.WithError(err).Debug("failed to add address to macvlan")
 		//implicitly deletes macvlan
-		err2 := vxl.Delete()
+		err2 := hi.vxl.Delete()
 		if err2 != nil {
 			log.WithError(err).WithError(err2).Debug("failed to delete vxlan")
 			return nil, err2
 		}
 		return nil, err
-	}
-
-	hi := &HostInterface{
-		name,
-		vxl,
-		mvl,
-		log,
 	}
 
 	return hi, nil
 }
 
 func GetHostInterface(name string) (*HostInterface, error) {
+	hi, err := getHostInterface(name)
+	if err != nil {
+		return nil, err
+	}
+	return hi, err
+}
+
+func getHostInterface(name string) (*HostInterface, error) {
 	log := log.WithField("HostInterface", name)
 	log.Debug("GetHostInterface")
 
-	vxl, err := vxlan.GetVxlan(name)
+	hi := &HostInterface{
+		name: name,
+		log:  log,
+	}
+
+	var err error
+	hi.vxl, err = vxlan.GetVxlan(name)
 	if err != nil {
 		log.Debug("failed to get vxlan interface")
-		return nil, err
+		return hi, err
 	}
 
-	mvl, err := macvlan.GetMacvlan("hmvl_" + name)
+	hi.mvl, err = macvlan.GetMacvlan("hmvl_" + name)
 	if err != nil {
 		log.Debug("failed to get macvlan interface")
-		return nil, err
 	}
 
-	hi := &HostInterface{
-		name,
-		vxl,
-		mvl,
-		log,
-	}
-
-	return hi, nil
-}
-
-func GetOrCreateHostInterface(name string, gateway *net.IPNet, opts map[string]string) (*HostInterface, error) {
-	log := log.WithField("HostInterface", name)
-	log.Debug("GetOrCreateHostInterface")
-	hi, err := GetHostInterface(name)
-	if err == nil {
-		return hi, nil
-	}
-
-	return NewHostInterface(name, gateway, opts)
+	return hi, err
 }
 
 func (hi *HostInterface) CreateMacvlan(name string) error {
