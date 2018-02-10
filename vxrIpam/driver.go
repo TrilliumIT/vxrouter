151bc48351f4f9fcbeedf5dae347ebcb224c686f
Clint Armstrong <clint@clintarmstrong.net>
2018-02-09 22:30:11 -0500

    structure logging in ipam

--- vxrIpam/driver.go
+++ vxrIpam/driver.go
@@ -13,36 +13,7 @@ type Driver struct {
 	respTime     time.Duration
 	excludeFirst int
 	excludeLast  int
-}
-
-func (d *Driver) GetCapabilities() (*ipam.CapabilitiesResponse, error) {
-	log.Debugf("vxrIpam.GetCapabilites()")
-	return &ipam.CapabilitiesResponse{}, nil
-}
-
-func (d *Driver) GetDefaultAddressSpaces() (*ipam.AddressSpacesResponse, error) {
-	log.Debugf("vxrIpam.GetDefaultAddressSpaces()")
-	return &ipam.AddressSpacesResponse{}, nil
-}
-
-func (d *Driver) RequestPool(r *ipam.RequestPoolRequest) (*ipam.RequestPoolResponse, error) {
-	log.WithField("r", r).Debugf("vxrIpam.RequestPool()")
-	return &ipam.RequestPoolResponse{}, nil
-}
-
-func (d *Driver) ReleasePool(r *ipam.ReleasePoolRequest) error {
-	log.WithField("r", r).Debugf("vxrIpam.ReleasePoolRequest()")
-	return nil
-}
-
-func (d *Driver) RequestAddress(r *ipam.RequestAddressRequest) (*ipam.RequestAddressResponse, error) {
-	log.WithField("r", r).Debugf("vxrIpam.RequestAddress()")
-	return &ipam.RequestAddressResponse{}, nil
-}
-
-func (d *Driver) ReleaseAddress(r *ipam.ReleaseAddressRequest) error {
-	log.WithField("r", r).Debugf("vxrIpam.ReleaseAddress()")
-	return nil
+	log          *log.Entry
 }
 
 func NewDriver(propTime, respTime time.Duration, excludeFirst, excludeLast int) (*Driver, error) {
@@ -51,6 +22,37 @@ func NewDriver(propTime, respTime time.Duration, excludeFirst, excludeLast int) 
 		respTime,
 		excludeFirst,
 		excludeLast,
+		log.WithField("driver", "vxrIpam"),
 	}
 	return d, nil
+}
+
+func (d *Driver) GetCapabilities() (*ipam.CapabilitiesResponse, error) {
+	d.log.Debug("GetCapabilites()")
+	return &ipam.CapabilitiesResponse{}, nil
+}
+
+func (d *Driver) GetDefaultAddressSpaces() (*ipam.AddressSpacesResponse, error) {
+	d.log.Debug("GetDefaultAddressSpaces()")
+	return &ipam.AddressSpacesResponse{}, nil
+}
+
+func (d *Driver) RequestPool(r *ipam.RequestPoolRequest) (*ipam.RequestPoolResponse, error) {
+	d.log.WithField("r", r).Debug("RequestPool()")
+	return &ipam.RequestPoolResponse{}, nil
+}
+
+func (d *Driver) ReleasePool(r *ipam.ReleasePoolRequest) error {
+	d.log.WithField("r", r).Debug("ReleasePoolRequest()")
+	return nil
+}
+
+func (d *Driver) RequestAddress(r *ipam.RequestAddressRequest) (*ipam.RequestAddressResponse, error) {
+	d.log.WithField("r", r).Debug("RequestAddress()")
+	return &ipam.RequestAddressResponse{}, nil
+}
+
+func (d *Driver) ReleaseAddress(r *ipam.ReleaseAddressRequest) error {
+	d.log.WithField("r", r).Debug("ReleaseAddress()")
+	return nil
 }
