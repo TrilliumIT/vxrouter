008797c41ea303a64bda50ddcfb7123fa9feb5e2
Travis Hegner <travis.hegner@gmail.com>
2018-02-16 15:06:29 -0500

    don't be dumb in bash

--- make.sh
+++ make.sh
@@ -4,4 +4,5 @@ echo "Linting..."
 gometalinter --skip=vendor --disable=gocyclo ./...
 
 echo "Building..."
-mkdir bin || go build -o bin/vxrnet ./docker/vxrnet
+mkdir bin || true
+go build -o bin/vxrnet ./docker/vxrnet
